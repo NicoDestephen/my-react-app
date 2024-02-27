582cfa8b2db14e5263e27f48683e0b236cdb3de0
NicoDestephen <ndestephen@gmail.com>
2024-02-27 10:52:30 +0100

    [UPDATE] PokemonCard.jsx to make it dynamic based on the date from the pokemon variable

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,31 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+];
+
 function PokemonCard() {
+    const pokemon = pokemonList[0];
     return (
         <figure className="card">
-            <img
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-                alt="bulbasaur"
-                className="card-img"
-            />
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.imgSrc ? (
+                <img
+                    src={pokemon.imgSrc}
+                    alt={pokemon.name}
+                    className="card-img"
+                />
+            ) : (
+                <p>???</p>
+            )}
+            
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
-  }
+}
   
-  export default PokemonCard;
+export default PokemonCard;
