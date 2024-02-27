649df1a684b3a17c4cd93f2ccb86d7b8e0594462
NicoDestephen <ndestephen@gmail.com>
2024-02-27 10:23:16 +0100

    add css to pokemon card

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,14 @@
 function PokemonCard() {
-    return  <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-                <figcaption>Bulbasaur</figcaption>
-            </figure>;
+    return (
+        <figure className="card">
+            <img
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+                alt="bulbasaur"
+                className="card-img"
+            />
+            <figcaption>Bulbasaur</figcaption>
+        </figure>
+    );
   }
   
   export default PokemonCard;
